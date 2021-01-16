7320bd8571f3c717e593f94300db177d4a08eea2
Luiz Victor Santos <68481812+luiz-victor-santos@users.noreply.github.com>
2021-01-16 21:52:57 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
+
+//Adding the encrypt pachadge to the new monggose Schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+// INSTALL NPM I DOTENV and check .env file to see the encryption code
+//Below we are adding the encrypt package as a plugin to the Schema and
+// is important to do it prior to create our mongoose model.
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); //-> Encrypt Only Certain Fields
+// //Secret String Instead of Two Keys
+
+// For convenience, you can also pass in a single secret string instead of two keys.
+// var secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
+// userSchema.plugin(encrypt, { secret: secret });
+//---------------------------------------------------------------------
+
+const User =  new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User ({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password =  req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log("Wrong user name or password!");
+    }else{
+      if (foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  });
+
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
